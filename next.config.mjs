4d5a332a4341247734d38f355d84c720a189eb75
Your Account A Username <leduyquang6@gmail.com>
2025-09-08 00:28:31 +0700

    update change nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,7 @@ const nextConfig = {
     domains: ["d31e5l6kryb7tc.cloudfront.net", "events.mapbox.com", "api.mapbox.com"],
   },
   async rewrites() {
-    if (!process.env.NEXT_PUBLIC_API_URL) {
-      return [];
-    }
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL.replace(/\/+$/,'')}/admin/:path*`,
-      },
-    ];
+    return [];
   },
 };
 
